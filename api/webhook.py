c6f04a002c5fa97a70e6ae3384d9c698df903285
SleepyCat <benson930417@gmail.com>
2026-03-28 03:20:33 +0100

    uyh7i

--- api/webhook.py
+++ api/webhook.py
@@ -117,6 +117,11 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text=reply_msg)
         )
+    elif user_msg == "欸嘿":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="欸嘿")
+        )
     else:
         line_bot_api.reply_message(
             event.reply_token,
